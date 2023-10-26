0173f748c5dc4e33668369ee40d2a57718d722f0
Gohar Ali <ghulammujtaba074@gmail.com>
2023-10-26 10:19:49 +0500

    itsme

---
+++ index.js
@@ -0,0 +1,19 @@
+var bulb = document.querySelector("#bulb");
+var btn = document.querySelector("#btn");
+
+let flag = 0;
+
+btn.addEventListener("click", function(){
+
+    if(flag == 0){
+        bulb.style.backgroundColor = "yellow";
+        btn.innerHTML = "OFF";
+        flag = 1;
+    }else{
+        bulb.style.backgroundColor = "transparent";
+        btn.innerHTML = "ON";
+        flag = 0;
+        
+    }
+    
+})
